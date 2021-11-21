38119ee6857e0792fa5ba1889677a4beaa02921d
Szymon Wysocki <szymon.wysocki.stud@pw.edu.pl>
2021-11-21 18:51:36 +0100

    add simple warehouse tree

--- src/main/java/edu/pw/pap21z/z15/ManagerController.java
+++ src/main/java/edu/pw/pap21z/z15/ManagerController.java
@@ -1,5 +1,39 @@
 package edu.pw.pap21z.z15;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+import java.util.List;
+
 public class ManagerController {
 
+    List<List<String>> aisle = List.of(
+            List.of("Półka 1", "Półka 2", "Półka 3"),
+            List.of("Półka 1", "Półka 2", "Półka 3"),
+            List.of("Półka 1", "Półka 2", "Półka 3")
+    );
+
+    @FXML
+    private TreeView<String> warehouseTree;
+
+    @FXML
+    private void initialize() {
+
+        TreeItem<String> warehouseItem = new TreeItem<>("Magazyn");
+
+        TreeItem<String> aisleItem = new TreeItem<>("Alejka");
+        for (List<String> rack : aisle) {
+            TreeItem<String> rackTreeItem = new TreeItem<>("Regał");
+            for (String level : rack) {
+                TreeItem<String> levelTreeItem = new TreeItem<>(level);
+                rackTreeItem.getChildren().add(levelTreeItem);
+            }
+            aisleItem.getChildren().add(rackTreeItem);
+        }
+
+        warehouseItem.getChildren().add(aisleItem);
+        warehouseTree.setShowRoot(false);
+        warehouseTree.setRoot(warehouseItem);
+    }
 }
