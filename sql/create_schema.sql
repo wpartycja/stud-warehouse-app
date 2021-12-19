2585ae20a08c69cde1c1b8391c76b32f2b919ec6
Szymon Wysocki <szymon.wysocki.stud@pw.edu.pl>
2021-12-19 23:55:19 +0100

    update db schema: follow naming conventions, migrate accounts.assigned_job to jobs.assigned_worker

--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -1,62 +1,63 @@
-DROP TABLE job;
-DROP TABLE clientorder;
-DROP TABLE pallet;
-DROP TABLE location;
-DROP TABLE account;
+DROP TABLE jobs CASCADE CONSTRAINTS;
+DROP TABLE orders CASCADE CONSTRAINTS;
+DROP TABLE pallets CASCADE CONSTRAINTS;
+DROP TABLE accounts CASCADE CONSTRAINTS;
+DROP TABLE locations CASCADE CONSTRAINTS;
 
-CREATE TABLE account
-(
-    username       VARCHAR(20) PRIMARY KEY,
-    password       VARCHAR(20) NOT NULL,
-    account_type   VARCHAR(20) NOT NULL,
-    name           VARCHAR(20),
-    surname        VARCHAR(20),
-    current_job_id INTEGER,
-    CONSTRAINT acc_type_valid CHECK ( account_type IN ('MANAGER', 'CLIENT', 'WORKER') )
-    -- todo: check only worker can have job
-);
 
-CREATE TABLE location
+CREATE TABLE locations
 (
-    location_id   INTEGER PRIMARY KEY,
-    location_type VARCHAR(20) NOT NULL,
-    path          VARCHAR(20) NOT NULL,
-    CONSTRAINT loc_type_valid CHECK ( location_type IN ('SHELF', 'IN_RAMP', 'OUT_RAMP') )
+    location_id INTEGER PRIMARY KEY,
+    type        VARCHAR(20) NOT NULL
+        CONSTRAINT location_type_enum CHECK ( type IN ('SHELF', 'IN_RAMP', 'OUT_RAMP') ),
+    path        VARCHAR(20) NOT NULL
     -- todo: check if shelf has max 1 item
 );
 
-CREATE TABLE pallet
+CREATE TABLE accounts
 (
-    pallet_id   INTEGER PRIMARY KEY,
-    description VARCHAR(20),
-    owner       VARCHAR(20) NOT NULL,
-    location_id INTEGER     NOT NULL,
-    CONSTRAINT owner_fk FOREIGN KEY (owner) REFERENCES account (username),
-    CONSTRAINT location_fk FOREIGN KEY (location_id) REFERENCES location (location_id)
+    account_username VARCHAR(20) PRIMARY KEY,
+    password         VARCHAR(20) NOT NULL,
+    type             VARCHAR(20) NOT NULL
+        CONSTRAINT account_type_enum CHECK ( type IN ('MANAGER', 'CLIENT', 'WORKER') ),
+    name             VARCHAR(20),
+    surname          VARCHAR(20)
+);
+
+CREATE TABLE pallets
+(
+    pallet_id      INTEGER PRIMARY KEY,
+    description    VARCHAR(20),
+    owner_username VARCHAR(20) NOT NULL
+        CONSTRAINT pal_own_fk REFERENCES accounts (account_username),
+    location_id    INTEGER     NOT NULL
+        CONSTRAINT location_fk REFERENCES locations (location_id)
 );
 
 -- todo: create table PalletType
 
-CREATE TABLE clientorder
+CREATE TABLE orders
 (
-    order_id   INTEGER PRIMARY KEY,
-    username   VARCHAR(20) NOT NULL,
-    order_type VARCHAR(20) NOT NULL,
-    CONSTRAINT client_fk FOREIGN KEY (username) REFERENCES account (username),
-    CONSTRAINT client_type_valid CHECK ( order_type IN ('IN', 'OUT') )
+    order_id        INTEGER PRIMARY KEY,
+    client_username VARCHAR(20) NOT NULL
+        CONSTRAINT order_client_fk REFERENCES accounts (account_username),
+    type            VARCHAR(20) NOT NULL
+        CONSTRAINT client_type_enum CHECK ( type IN ('IN', 'OUT') )
     -- todo check if account is client type
 );
 
-CREATE TABLE job
+CREATE TABLE jobs
 (
-    job_id         INTEGER PRIMARY KEY,
-    destination_id INTEGER     NOT NULL,
-    pallet_id      INTEGER     NOT NULL,
-    order_id       INTEGER     NOT NULL,
-    status         VARCHAR(20) NOT NULL,
-    CONSTRAINT dest_fk FOREIGN KEY (destination_id) REFERENCES location (location_id),
-    CONSTRAINT pallet_fk FOREIGN KEY (pallet_id) REFERENCES pallet (pallet_id),
-    CONSTRAINT order_fk FOREIGN KEY (order_id) REFERENCES clientorder (order_id),
-    CONSTRAINT status_valid CHECK ( status IN ('PLANNED', 'PENDING', 'IN_PROGRESS', 'COMPLETED') )
-    -- todo check if job has max 1 worker
+    job_id                   INTEGER PRIMARY KEY,
+    destination_id           INTEGER     NOT NULL
+        CONSTRAINT job_destination_fk REFERENCES locations (location_id),
+    pallet_id                INTEGER     NOT NULL
+        CONSTRAINT job_pallet_fk REFERENCES pallets (pallet_id),
+    order_id                 INTEGER     NOT NULL
+        CONSTRAINT job_order_fk REFERENCES orders (order_id),
+    status                   VARCHAR(20) NOT NULL
+        CONSTRAINT job_status_enum CHECK ( status IN ('PLANNED', 'PENDING', 'IN_PROGRESS', 'COMPLETED') ),
+    assigned_worker_username VARCHAR(20)
+        CONSTRAINT job_assigned_worker_fk REFERENCES accounts (account_username)
+    -- todo: check only worker can have job
 );
--- sql/insert_sample_values.sql
+++ sql/insert_sample_values.sql
@@ -1,46 +1,46 @@
-TRUNCATE TABLE job;
-TRUNCATE TABLE clientorder;
-TRUNCATE TABLE pallet;
-TRUNCATE TABLE location;
-TRUNCATE TABLE account;
+TRUNCATE TABLE jobs;
+TRUNCATE TABLE orders;
+TRUNCATE TABLE pallets;
+TRUNCATE TABLE locations;
+TRUNCATE TABLE accounts;
 
-INSERT INTO z15.location (location_id, location_type, path)
+INSERT INTO z15.locations (location_id, type, path)
 VALUES (1, 'SHELF', 'A/1/2');
-INSERT INTO z15.location (location_id, location_type, path)
+INSERT INTO z15.locations (location_id, type, path)
 VALUES (2, 'SHELF', 'A/1/3');
-INSERT INTO z15.location (location_id, location_type, path)
+INSERT INTO z15.locations (location_id, type, path)
 VALUES (3, 'IN_RAMP', 'In ramp 1');
-INSERT INTO z15.location (location_id, location_type, path)
+INSERT INTO z15.locations (location_id, type, path)
 VALUES (4, 'OUT_RAMP', 'Out ramp 1');
 
-INSERT INTO z15.account (username, password, account_type, name, surname, current_job_id)
-VALUES ('c1', 'pass', 'CLIENT', 'Karol', 'Kurka', NULL);
-INSERT INTO z15.account (username, password, account_type, name, surname, current_job_id)
-VALUES ('c2', 'pass', 'CLIENT', 'Karolina', NULL, NULL);
-INSERT INTO z15.account (username, password, account_type, name, surname, current_job_id)
-VALUES ('m1', 'pass', 'MANAGER', 'Michał', 'Malinowski', NULL);
-INSERT INTO z15.account (username, password, account_type, name, surname, current_job_id)
-VALUES ('m2', 'pass', 'MANAGER', 'Marcin', 'Modrzejewski', NULL);
-INSERT INTO z15.account (username, password, account_type, name, surname, current_job_id)
-VALUES ('w1', 'pass', 'WORKER', 'Wiesław', 'Wrona', NULL);
-INSERT INTO z15.account (username, password, account_type, name, surname, current_job_id)
-VALUES ('w2', 'pass', 'WORKER', 'Wojciech', 'Wysocki', NULL);
+INSERT INTO z15.accounts (account_username, password, type, name, surname)
+VALUES ('c1', 'pass', 'CLIENT', 'Karol', 'Kurka');
+INSERT INTO z15.accounts (account_username, password, type, name, surname)
+VALUES ('c2', 'pass', 'CLIENT', 'Karolina', NULL);
+INSERT INTO z15.accounts (account_username, password, type, name, surname)
+VALUES ('m1', 'pass', 'MANAGER', 'Michał', 'Malinowski');
+INSERT INTO z15.accounts (account_username, password, type, name, surname)
+VALUES ('m2', 'pass', 'MANAGER', 'Marcin', 'Modrzejewski');
+INSERT INTO z15.accounts (account_username, password, type, name, surname)
+VALUES ('w1', 'pass', 'WORKER', 'Wiesław', 'Wrona');
+INSERT INTO z15.accounts (account_username, password, type, name, surname)
+VALUES ('w2', 'pass', 'WORKER', 'Wojciech', 'Wysocki');
 
-INSERT INTO z15.pallet (pallet_id, description, owner, location_id)
+INSERT INTO z15.pallets (pallet_id, description, owner_username, location_id)
 VALUES (2, 'Krzesła', 'c1', 2);
-INSERT INTO z15.pallet (pallet_id, description, owner, location_id)
+INSERT INTO z15.pallets (pallet_id, description, owner_username, location_id)
 VALUES (3, 'Czekolada', 'c2', 4);
-INSERT INTO z15.pallet (pallet_id, description, owner, location_id)
+INSERT INTO z15.pallets (pallet_id, description, owner_username, location_id)
 VALUES (4, 'Grzejnik', 'c1', 4);
 
-INSERT INTO z15.clientorder (order_id, username, order_type)
+INSERT INTO z15.orders (order_id, client_username, type)
 VALUES (1, 'c1', 'IN');
-INSERT INTO z15.clientorder (order_id, username, order_type)
+INSERT INTO z15.orders (order_id, client_username, type)
 VALUES (2, 'c2', 'IN');
 
-INSERT INTO z15.job (job_id, destination_id, pallet_id, order_id, status)
-VALUES (1, 1, 4, 1, 'PENDING');
-INSERT INTO z15.job (job_id, destination_id, pallet_id, order_id, status)
-VALUES (2, 2, 2, 1, 'PENDING');
-INSERT INTO z15.job (job_id, destination_id, pallet_id, order_id, status)
-VALUES (3, 3, 3, 2, 'PENDING');
+INSERT INTO z15.jobs (job_id, destination_id, pallet_id, order_id, status, assigned_worker_username)
+VALUES (1, 1, 4, 1, 'PENDING', NULL);
+INSERT INTO z15.jobs (job_id, destination_id, pallet_id, order_id, status, assigned_worker_username)
+VALUES (2, 2, 2, 1, 'PENDING', NULL);
+INSERT INTO z15.jobs (job_id, destination_id, pallet_id, order_id, status, assigned_worker_username)
+VALUES (3, 3, 3, 2, 'PENDING', NULL);
