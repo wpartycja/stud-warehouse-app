93bcf18bb535a25ff379a77e05c1d889cc927f86
Szymon Wysocki <szymon.wysocki.stud@pw.edu.pl>
2022-01-08 11:27:49 +0100

    remove ManagerRepository

--- src/main/java/edu/pw/pap21z/z15/db/Database.java
+++ src/main/java/edu/pw/pap21z/z15/db/Database.java
@@ -1,15 +1,11 @@
 package edu.pw.pap21z.z15.db;
 
-import edu.pw.pap21z.z15.db.model.Account;
-import edu.pw.pap21z.z15.db.model.Job;
-import edu.pw.pap21z.z15.db.model.Location;
-import edu.pw.pap21z.z15.db.model.Pallet;
+import edu.pw.pap21z.z15.db.model.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // todo abstract into DAOs and repositories for each entity type
 public class Database {
@@ -39,15 +35,49 @@ public class Database {
         return getAll(Location.class);
     }
 
-    public List<Account> getWorkers() {
-        return getAll(Account.class); // todo: should be just accounts with type worker
-    }
-
     public List<Job> getJobs() {
         return getAll(Job.class); // todo shouldn't probably return completed jobs by default
     }
 
     public Job getJobById(Long id) {
         return getById(Job.class, id);
+    }
+
+
+    public List<Order> getOrders() {
+        return getAll(Order.class);
+    }
+
+    public List<Account> getWorkers() {
+        TypedQuery<Account> query = session.createQuery("SELECT a from Account a where a.type = edu.pw.pap21z.z15.db.model.AccountType.WORKER", Account.class);
+        return query.getResultList();
+    }
+
+    public List<Location> getAvailableDestinations(Job job) {
+        if (job.getOrder().getType() == OrderType.IN) {
+            TypedQuery<Location> query = session.createQuery("SELECT l from Location l where l.type = edu.pw.pap21z.z15.db.model.LocationType.SHELF", Location.class);
+            var shelves = query.getResultList();
+            return shelves.stream()
+                    .filter(s -> s.getPallets().isEmpty())
+                    .collect(Collectors.toList());
+        } else {
+            TypedQuery<Location> query = session.createQuery("SELECT l from Location l where l.type = edu.pw.pap21z.z15.db.model.LocationType.OUT_RAMP", Location.class);
+            return query.getResultList();
+        }
+    }
+
+    public void scheduleJob(Job job, Location dest) {
+        EntityTransaction transaction = session.getTransaction();
+        transaction.begin();
+        job.setStatus(JobStatus.PENDING);
+        job.setDestination(dest);
+        transaction.commit();
+    }
+
+    public void unscheduleJob(Job job) {
+        EntityTransaction transaction = session.getTransaction();
+        transaction.begin();
+        job.setStatus(JobStatus.PLANNED);
+        transaction.commit();
     }
 }
